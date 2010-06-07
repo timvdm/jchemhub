e13d51c19202baf26b66cf5487b6979fe369ee0a
Paul Novak <novakps@gmail.com>
2010-06-07 09:13:54 -0400

    fix bond highlight

--- jchemhub/view/molecule_renderer.js
+++ jchemhub/view/molecule_renderer.js
@@ -25,7 +25,7 @@ goog.inherits(jchemhub.view.MoleculeRenderer, jchemhub.view.Renderer);
 jchemhub.view.MoleculeRenderer.prototype.render = function(molecule, trans, group) {
 	this.transform = trans;
 	goog.array.forEach(molecule.bonds, function(bond) {
-		this.bondRendererFactory.get(bond).render(bond, trans, group);
+		this.bondRendererFactory.get(bond).render(bond, trans, undefined);
 	}, this);
 	goog.array.forEach(molecule.atoms, function(atom) {
 		this.atomRenderer.render(atom, trans, this.atomController);
