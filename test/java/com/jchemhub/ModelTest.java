3b7caea5dceb0b58cce88620eb28d40df8f9dac1
Paul Novak <novakps@gmail.com>
2010-06-08 08:23:44 -0400

    implement add bond fundtionality

--- test/java/com/jchemhub/ModelTest.java
+++ test/java/com/jchemhub/ModelTest.java
@@ -1,14 +1,14 @@
 package com.jchemhub; 
 import com.thoughtworks.selenium.SeleneseTestCase; 
 public class ModelTest extends SeleneseTestCase { 
-    private final String DIRECTORY_PREFIX = "file:////Users/paulnovak/wingu/workspace/jchemhub";
+    private final String DIRECTORY_PREFIX = "file:////Users/paulnovak/wingu/workspace/jchemhub/";
   
   public void setUp() throws Exception { 
     final String url = DIRECTORY_PREFIX; 
     final String browserString = "*firefox"; 
     setUp(url, browserString); 
   } 
-  public void testGoogString() throws Exception { 
+  public void testModel() throws Exception { 
     selenium.open(DIRECTORY_PREFIX + "jchemhub/model/model_test.html"); 
     selenium.waitForCondition( 
         "window.G_testRunner && window.G_testRunner.isFinished()", 
